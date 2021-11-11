afc64923dcee801ac32945ff4c3d9db41a33263f
cdxk <928566418@qq.com>
2021-11-11 22:15:19 +0800

    调试1

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from testSuite import testSuite
+import os
 import sys
-sys.path.append("..")
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+PathProject = os.path.split(rootPath)[0]
+sys.path.append(rootPath)
+sys.path.append(PathProject)
+import testSuite.testSuite
