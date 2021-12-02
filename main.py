a68eceb938885922a67462ce7b24123fe8a2e53c
cdxk <928566418@qq.com>
2021-12-02 16:58:13 +0800

    测试故障接口

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ rootPath = os.path.split(curPath)[0]
 PathProject = os.path.split(rootPath)[0]
 sys.path.append(rootPath)
 sys.path.append(PathProject)
-print("=====构建===开始====")
 
-print("=====构建===结束====")
 
 if __name__=='__main__':
+    print("=====构建===开始====")
     TestSuite().run()
+    print("=====构建===结束====")
+
