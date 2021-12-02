3474dbf3ccc640abf1680090704d0e7e50f60098
cdxk <928566418@qq.com>
2021-12-02 11:55:25 +0800

    测试故障接口

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import sys
-import testSuite.testSuite
+from testSuite.testSuite import TestSuite
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 PathProject = os.path.split(rootPath)[0]
 sys.path.append(rootPath)
 sys.path.append(PathProject)
 print("=====构建===开始====")
-testSuite.testSuite.TestSuite().run()
+TestSuite().run()
 print("=====构建===结束====")
