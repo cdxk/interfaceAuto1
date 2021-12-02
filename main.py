3c79de0394c8468fc24b8c9666f2e8fec42ce323
cdxk <928566418@qq.com>
2021-12-02 10:17:55 +0800

    main调用testsuite

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import sys
-
+import testSuite.testSuite
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 PathProject = os.path.split(rootPath)[0]
 sys.path.append(rootPath)
 sys.path.append(PathProject)
 print("=====构建===开始====")
-import testSuite.testSuite
+testSuite.testSuite.TestSuite().run()
+print("=====构建===结束====")
