7696e316c043df6ca57913a6c69b8c7b7fa947b6
caidan <928566418@qq.com>
2021-12-14 10:19:48 +0800

    调整发送邮件类

--- common/SendEmail.py
+++ common/SendEmail.py
@@ -8,37 +8,45 @@ from util import get_path
 from email.mime.application import MIMEApplication
 from common.log import logs
 
-msg_from=readConfig.ReadConfig().get_config("EMAIL","from")
-msg_password=readConfig.ReadConfig().get_config("EMAIL","authorizationcode")
-msg_to=readConfig.ReadConfig().get_config("EMAIL","to")
-msg_content=readConfig.ReadConfig().get_config("EMAIL","content")
-#获取html文件内容
-f=open(get_path.getPath()+"result/report.html","rb")
-send_html=f.read()
-f.close()
+class SendEmail():
+    def sendemail(self):
+        msg_from = readConfig.ReadConfig().get_config("EMAIL", "from")
+        msg_password = readConfig.ReadConfig().get_config("EMAIL", "authorizationcode")
+        msg_to = readConfig.ReadConfig().get_config("EMAIL", "to")
+        msg_content = readConfig.ReadConfig().get_config("EMAIL", "content")
+        # 获取html文件内容
+        f = open(get_path.getPath() + "result/report.html", "rb")
+        send_html = f.read()
+        f.close()
 
-msg=MIMEMultipart()
-msg.attach(MIMEText(send_html,'html','utf-8'))
-msg['from']=readConfig.ReadConfig().get_config("EMAIL","from")
-msg['Subject']=readConfig.ReadConfig().get_config("EMAIL","subject")
-msg['To']=readConfig.ReadConfig().get_config("EMAIL","to")
+        msg = MIMEMultipart()
+        msg.attach(MIMEText(send_html, 'html', 'utf-8'))
+        msg['from'] = readConfig.ReadConfig().get_config("EMAIL", "from")
+        msg['Subject'] = readConfig.ReadConfig().get_config("EMAIL", "subject")
+        msg['To'] = readConfig.ReadConfig().get_config("EMAIL", "to")
 
-#加附件
-#send_file=open(get_path.getPath()+"result/report.html","rb").read()
-att=MIMEText(send_html,'HTML','UTF-8')
-att['Content-Type']='application/octet-stream'
-att.add_header('Content-Disposition','attachment',filename="附件-自动化测试报告")
-msg.attach(att)
+        # 加附件
+        # send_file=open(get_path.getPath()+"result/report.html","rb").read()
+        att = MIMEText(send_html, 'HTML', 'UTF-8')
+        att['Content-Type'] = 'application/octet-stream'
+        att.add_header('Content-Disposition', 'attachment', filename="附件-自动化测试报告")
+        msg.attach(att)
+
+        try:
+            s = smtplib.SMTP_SSL(readConfig.ReadConfig().get_config("EMAIL", "sendserver"),
+                                 readConfig.ReadConfig().get_config("EMAIL", "port"))
+            s.login(msg_from, msg_password)
+            s.sendmail(msg_from, msg_to, msg.as_string())
+            logs.info("=======邮件发送成功======")
+            print("发送成功")
+        except smtplib.SMTPException as e:
+            logs.error("=======邮件发送失败======" + e)
+            print(e)
+        finally:
+            s.quit()
 
 
-try:
-    s=smtplib.SMTP_SSL(readConfig.ReadConfig().get_config("EMAIL","sendserver"),readConfig.ReadConfig().get_config("EMAIL","port"))
-    s.login(msg_from,msg_password)
-    s.sendmail(msg_from,msg_to,msg.as_string())
-    logs.info("=======邮件发送成功======")
-    print("发送成功")
-except smtplib.SMTPException as e:
-    logs.error("=======邮件发送失败======"+e)
-    print (e)
-finally:
-    s.quit()
+
+if __name__=='__main__':
+    SendEmail.sendemail()
+
--- testCase/faultAlarm.py
+++ testCase/faultAlarm.py
@@ -27,19 +27,19 @@ class TestFaultAlarm(unittest.TestCase):
         logs.info("故障告警工单测试结束。。。")
 
     #工单全部通过流程（需领导审核流程）
-    # def test_allagree(self):
-    #     try:
-    #         self.dispatch.addAndCommit('创建提交领导审核')
-    #         self.dispatch.approve('作业区值班领导同意')
-    #         self.dispatch.approve('厂调审核需领导审核')
-    #         self.dispatch.approve('生产运行科同意且需领导')
-    #         self.dispatch.approve('厂级领导发表意见')
-    #         self.dispatch.approve('录入处理意见')
-    #         logs.info("===故障工单流程全部通过处理完毕===")
-    #     except Exception:
-    #         print('流程出错')
-    #         logs.error("===故障工单流程全部通过处理出错===")
-    #         raise
+    def test_allagree(self):
+        try:
+            self.dispatch.addAndCommit('创建提交领导审核')
+            self.dispatch.approve('作业区值班领导同意')
+            self.dispatch.approve('厂调审核需领导审核')
+            self.dispatch.approve('生产运行科同意且需领导')
+            self.dispatch.approve('厂级领导发表意见')
+            self.dispatch.approve('录入处理意见')
+            logs.info("===故障工单流程全部通过处理完毕===")
+        except Exception:
+            print('流程出错')
+            logs.error("===故障工单流程全部通过处理出错===")
+            raise
 
     #不需厂领导录意见
     def test_allagree(self):
@@ -47,10 +47,9 @@ class TestFaultAlarm(unittest.TestCase):
             print("============创建故障工单====")
             self.dispatch.addAndCommit('创建提交领导审核')
             self.dispatch.approve('作业区值班领导同意')
-            print("用例执行完毕")
-            # self.dispatch.approve('厂调审核需领导审核')
-            # self.dispatch.approve('生产运行科同意无需领导')
-            # self.dispatch.approve('录入处理意见')
+            self.dispatch.approve('厂调审核需领导审核')
+            self.dispatch.approve('生产运行科同意无需领导')
+            self.dispatch.approve('录入处理意见')
             logs.info("===故障工单流程全部通过处理完毕===")
         except Exception:
             print('流程出错')
@@ -58,54 +57,53 @@ class TestFaultAlarm(unittest.TestCase):
             raise
 
     # 创建人-厂调-结束
-    # def test_short(self):
-    #     try:
-    #         self.dispatch.addAndCommit('创建提交厂调审核')
-    #         self.dispatch.approve('厂调审核无需领导审核')
-    #         logs.info("===创建人-厂调-结束流程全部通过处理完毕===")
-    #     except Exception:
-    #         logs.error("===创建人-厂调-结束流程全部通过处理报错===")
-    #         raise
+    def test_short(self):
+        try:
+            self.dispatch.addAndCommit('创建提交厂调审核')
+            self.dispatch.approve('厂调审核无需领导审核')
+            logs.info("===创建人-厂调-结束流程全部通过处理完毕===")
+        except Exception:
+            logs.error("===创建人-厂调-结束流程全部通过处理报错===")
+            raise
 
     #作业区领导驳回
-    # def test_applyLeader_refused(self):
-    #     try:
-    #         self.dispatch.addAndCommit('创建提交领导审核')
-    #         self.dispatch.approve('作业区值班领导驳回')
-    #         logs.info("===故障工单作业区领导驳回完成===")
-    #     except Exception:
-    #         logs.error("===故障工单作业区领导驳回完出错==")
-    #         raise
+    def test_applyLeader_refused(self):
+        try:
+            self.dispatch.addAndCommit('创建提交领导审核')
+            self.dispatch.approve('作业区值班领导驳回')
+            logs.info("===故障工单作业区领导驳回完成===")
+        except Exception:
+            logs.error("===故障工单作业区领导驳回完出错==")
+            raise
 
     #生产运行科驳回
-    # def test_productionLeader_refused(self):
-    #     try:
-    #         self.dispatch.addAndCommit('创建提交领导审核')
-    #         self.dispatch.approve('作业区值班领导同意')
-    #         self.dispatch.approve('厂调审核需领导审核')
-    #         self.dispatch.approve('生产运行科驳回')
-    #         logs.info("===生产运行科驳回处理完毕===")
-    #     except Exception:
-    #         logs.error("===生产运行科驳回处理报错===")
-    #         raise
-
+    def test_productionLeader_refused(self):
+        try:
+            self.dispatch.addAndCommit('创建提交领导审核')
+            self.dispatch.approve('作业区值班领导同意')
+            self.dispatch.approve('厂调审核需领导审核')
+            self.dispatch.approve('生产运行科驳回')
+            logs.info("===生产运行科驳回处理完毕===")
+        except Exception:
+            logs.error("===生产运行科驳回处理报错===")
+            raise
     #驳回关闭
-    # def test_factoryLeader_refused(self):
-    #     try:
-    #         self.dispatch.addAndCommit('创建提交领导审核')
-    #         self.dispatch.approve('作业区值班领导驳回')
-    #         self.dispatch.closedata('关闭')
-    #         logs.info("===关闭故障工单完毕===")
-    #     except Exception:
-    #         logs.error("===关闭故障工单报错===")
-    #         raise
-
-    # def test_addDraft(self):
-    #     try:
-    #         self.dispatch.addAndCommit('创建草稿')
-    #     except Exception:
-    #         print('流程出错')
-    #         raise
+    def test_factoryLeader_refused(self):
+        try:
+            self.dispatch.addAndCommit('创建提交领导审核')
+            self.dispatch.approve('作业区值班领导驳回')
+            self.dispatch.closedata('关闭')
+            logs.info("===关闭故障工单完毕===")
+        except Exception:
+            logs.error("===关闭故障工单报错===")
+            raise
+    #创建草稿
+    def test_addDraft(self):
+        try:
+            self.dispatch.addAndCommit('创建草稿')
+        except Exception:
+            print('流程出错')
+            raise
 
 
 
